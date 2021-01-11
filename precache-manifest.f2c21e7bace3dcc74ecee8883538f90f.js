37f49bc691e61c45df5639b60b6b1e89465e3f66
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-11 01:03:46 +0000

    Deploy LinYenCheng/thsr-app to github.com/LinYenCheng/thsr-app.git:gh-pages

---
+++ precache-manifest.f2c21e7bace3dcc74ecee8883538f90f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d1c0eff7abc9b87d3d542a5331fa62",
+    "url": "/thsr-app/index.html"
+  },
+  {
+    "revision": "4c4afcb02caced6b24ae",
+    "url": "/thsr-app/static/css/main.fd0281ec.chunk.css"
+  },
+  {
+    "revision": "f1d617e8007c76ac03dc",
+    "url": "/thsr-app/static/js/2.e1be9ad1.chunk.js"
+  },
+  {
+    "revision": "ef6214a31d94d1146716b5b9f93b9ecb",
+    "url": "/thsr-app/static/js/2.e1be9ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4afcb02caced6b24ae",
+    "url": "/thsr-app/static/js/main.116d536c.chunk.js"
+  },
+  {
+    "revision": "a12cddfa300f8a975eb7",
+    "url": "/thsr-app/static/js/runtime-main.4af53cd8.js"
+  }
+]);
